492b388cb8e086d66ba373de5508c0d642b2ee19
SorinGreu23 <greusorin2003@gmail.com>
2021-10-21 23:31:12 +0300

    API Generic Repository Implementation

--- Infrastructure/Data/Migrations/20211021201823_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20211021201823_InitialCreate.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20211019132251_InitialCreate")]
+    [Migration("20211021201823_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
