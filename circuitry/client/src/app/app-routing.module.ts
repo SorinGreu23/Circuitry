c050f32bcea6654eceb2c39eb9de691c9a2740d4
Sorin Greu <greusorin2003@gmail.com>
2022-01-11 14:47:51 +0200

    Error handling added for client-side

--- circuitry/client/src/app/app-routing.module.ts
+++ circuitry/client/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
@@ -9,6 +12,9 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: 'about', component: AboutComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
